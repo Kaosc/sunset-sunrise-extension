76e3f6593852cd4be9f3247ced85733bbb7c5a66
Kaosc <themelihbt@gmail.com>
2023-01-17 12:30:03 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,26 +1,65 @@
-import { useEffect, useState } from "react"
+import { useCallback, useEffect, useState, useRef } from "react"
 import "./styles/App.css"
 
 export default function App() {
-	const [city] = useState({
-		id: 37517,
-		name: "Ottawa",
-		lat: 38.6,
-		lng: -95.2642,
-	})
+	const [city, setCity] = useState({})
+	const [times, setTimes] = useState({})
+	const [cities, setCities] = useState([])
+	const inputRef = useRef(null)
 
-	const [response, setResponse] = useState({})
 	useEffect(() => {
-		fetch(`https://api.sunrise-sunset.org/json?lat=${city.lat}&lng=${city.lng}&date=today`)
+		if (cities.length > 0) return
+		fetch("https://raw.githubusercontent.com/Kaosc/cities-geo-location-api/master/cities.json")
 			.then((response) => response.json())
-			.then((data) => setResponse(data.results))
+			.then((data) => setCities(data))
+	}, [])
+
+	const searchHandler = (e) => {
+		if (e.key === "Enter" && e.target.value !== "") {
+			const filteredCity = cities.find((city) => {
+				return city.name.toLowerCase().includes(e.target.value.toLowerCase())
+			})
+
+			if (filteredCity) {
+				fetch(
+					`https://api.sunrise-sunset.org/json?lat=${filteredCity.lat}&lng=${filteredCity.lng}&date=today`
+				)
+					.then((response) => response.json())
+					.then((data) => {
+						setCity(filteredCity)
+						setTimes(data.results)
+						inputRef.current.value = ""
+					})
+			}
+		}
+	}
+
+	const RenderCity = useCallback(() => {
+		if (!city.name) return <h1 style={{ paddingLeft: "30px", color: "#b8b8b83d" }}>Zzz</h1>
+		return (
+			<div style={{ paddingLeft: "20px" }}>
+				<h3 style={{ marginBottom: "15px" }}>{city.name}</h3>
+				<h4 style={{ marginBottom: "0" }}>Sunrise: {times.sunrise}</h4>
+				<h4>Sunset: {times.sunset}</h4>
+			</div>
+		)
 	}, [city])
 
 	return (
 		<div className='App'>
-			<h1>{city.name}</h1>
-			<h2>Sunrise: {response.sunrise}</h2>
-			<h2>Sunset: {response.sunset}</h2>
+			<main className='container'>
+				<input
+					ref={inputRef}
+					autoFocust={true}
+					style={{ margin: "20px", width: "300px", height: "50px" }}
+					type='search'
+					id='search'
+					name='search'
+					placeholder='Search City'
+					onKeyDown={searchHandler}
+				/>
+				<RenderCity />
+			</main>
 		</div>
 	)
 }
